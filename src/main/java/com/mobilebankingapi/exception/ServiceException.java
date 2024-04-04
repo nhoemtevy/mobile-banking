41dd4174c6f1a10a06b3dbbcf69ba2eb60018526
tevynhoem <tevymomnh193@gmail.com>
2024-04-04 10:01:54 +0700

    homework3

--- src/main/java/com/mobilebankingapi/exception/ServiceException.java
+++ src/main/java/com/mobilebankingapi/exception/ServiceException.java
@@ -1,4 +1,20 @@
 package com.mobilebankingapi.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+@RestControllerAdvice
 public class ServiceException {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    ResponseEntity<?> handleServiceErrors(ResponseStatusException ex) {
+
+        return ResponseEntity.status(ex.getStatusCode())
+                .body(Map.of("errors", ex.getReason()));
+    }
+
 }
+
